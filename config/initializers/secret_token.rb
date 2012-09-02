f56bc42feb21f6acc275d0d8bce6f103708a663f
Wind <windlongfei@gmail.com>
2012-09-02 10:45:19 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetPetBook::Application.config.secret_token = '9fd0a7ff1f307c0f8e77f1790688f047ffb0ec8a51780ba0190efc4d51ecea3c01804ba61e758d6c68c19b5d790ef8f9505d9b2646437cc508997e4d1c02e174'
